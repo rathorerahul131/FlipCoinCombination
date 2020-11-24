b48b2d44bc5727239d5eb72ad3d1f98f4fec9f9c
neelrahulr <rathorerahul131@gmail.com>
2020-11-24 15:38:21 +0530

    Refactored:Calculated Singlet Percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -2,12 +2,33 @@
 
 echo "Welcome to the Flip Coin Simulation Game"
 
-toss=$((RANDOM%2)) # toss will randomly store either 0 or 1
+noOfHead=0 #variable to store the count of Head wins
+noOfTail=0 #variable to store the count of Tail wins
+declare -A TossStore
+for ((i=0;i<20;i++))
+do
+	toss=$((RANDOM%2)) # toss will randomly store either 0 or 1
+	
+	if [[ $toss -eq 0 ]]
+	then	
+		TossStore[$i]="T"
+		((noOfTail++)) 
+	else
+		TossStore[$i]="H"
+		((noOfHead++))
+	fi	
+done
 
-#condition to check who wins
-if [[ $toss -eq 0 ]]
-then
-	echo "Tail Wins" 
-else
-	echo "Head Wins"
-fi
+echo "Head has won $noOfHead times"
+echo "Tail has won $noOfTail times"
+
+echo ${TossStore[@]}
+
+HeadAvg=`awk 'BEGIN{print ('$noOfHead'/21) }'`
+TailAvg=`awk 'BEGIN{print ('$noOfTail'/21) }'`
+
+HeadPercent=`awk 'BEGIN{print ('$HeadAvg' * 100)}'`
+TailPercent=`awk 'BEGIN{print ('$TailAvg' * 100)}'`
+
+echo "Head Percetage is $HeadPercent"
+echo "Tail Percetage is $TailPercent"
