9e86c316054648045190b6ba3befada5ee6b8d6f
neelrahulr <rathorerahul131@gmail.com>
2020-11-24 15:34:54 +0530

    Fliped the coin once

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
 echo "Welcome to the Flip Coin Simulation Game"
+
+toss=$((RANDOM%2)) # toss will randomly store either 0 or 1
+
+#condition to check who wins
+if [[ $toss -eq 0 ]]
+then
+	echo "Tail Wins" 
+else
+	echo "Head Wins"
+fi
