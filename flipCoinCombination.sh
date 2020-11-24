d6b475924f589431b9bca662f5d7113007a9a4f4
neelrahulr <rathorerahul131@gmail.com>
2020-11-24 15:54:42 +0530

    Refactored: Generalized for multiple flips and coins

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -5,79 +5,55 @@ echo "Welcome to the Flip Coin Simulation Game"
 echo "Enter the no. of flips"
 read flips
 
-echo "Enter the no of coins either 1 or 2 "
+echo "Enter the no of coins"
 read coins
 
 noOfHead=0 #variable to store the count of Head wins
 noOfTail=0 #variable to store the count of Tail wins
+
 declare -A TossStore
-if [[ $coins -eq 1 ]]
-then
 
-	for (( i=0;i<$flips;i++ ))
+#max is variable to store max count of combinations
+max=0 
+
+#winCombo is the varibale to store the winning Combination
+winCombo=""
+
+#loop till number of flips required
+
+for (( i=0;i<$flips;i++ )) 
+do 
+combo=""
+	for (( j=0;j<$coins;j++ )) #it will Create the combination 
 	do
-		toss=$(( RANDOM%2 )) # toss will randomly store either 0 or 1
-	
+		toss=$(( RANDOM%2 ))
 		if [[ $toss -eq 0 ]]
-		then	
-			TossStore[$i]="T"
-			((noOfTail++)) 
+		then
+			combo=$combo"T"
+			((noOfTail++))
 		else
-			TossStore[$i]="H"
+			combo=$combo"H"
 			((noOfHead++))
-		fi	
+
+		fi
 	done
 
-	echo "Head has won $noOfHead times"
-	echo "Tail has won $noOfTail times"
+#Increasing the count of particular Combination and storing in the dictionary
 
-	echo ${TossStore[@]}
+TossStore["$combo"]=$(( ${TossStore["$combo"]} + 1 ))
 
-	HeadAvg=`awk 'BEGIN{print ('$noOfHead'/21) }'`
-	TailAvg=`awk 'BEGIN{print ('$noOfTail'/21) }'`
+#condition to get the maximum occured combination
 
-	HeadPercent=`awk 'BEGIN{print ('$HeadAvg' * 100)}'`
-	TailPercent=`awk 'BEGIN{print ('$TailAvg' * 100)}'`
-
-	echo "Head Percetage is $HeadPercent"
-	echo "Tail Percetage is $TailPercent"
-
-elif [[ $coins -eq 2 ]]
+if [[ $max -lt ${TossStore["$combo"]} ]]
 then
-
-	echo "you choosed Two coins"
-	max=0
-	winCombo=""
-	for (( i=0;i<$flips;i++ ))
-	do 
-	combo=""
-		for (( j=0;j<$coins;j++ ))
-		do
-			toss=$(( RANDOM%2 ))
-			if [[ $toss -eq 0 ]]
-			then
-				combo=$combo"T"
-			else
-				combo=$combo"H"
-
-			fi
-		done
-
-	TossStore["$combo"]=$(( ${TossStore["$combo"]} + 1 ))
-	if [[ $max -lt ${TossStore["$combo"]} ]]
-	then
-		max=${TossStore["$combo"]}
-		winCombo="$combo"
-	fi
-
-	done
-
-	echo ${!TossStore[@]}
-	echo ${TossStore[@]}
-
-	echo "The winning Combination is $winCombo It occurred $max times"
-	
-else
-	echo "Enter either 1 or 2 coins only"
-
+	max=${TossStore["$combo"]}
+	winCombo="$combo"
 fi
+done
+
+echo "The Combinations are ${!TossStore[@]}"
+echo "The number of times each combination occured are ${TossStore[@]}"
+
+echo "The winning Combination is $winCombo" 
+echo "It occurred $max times"
+	
